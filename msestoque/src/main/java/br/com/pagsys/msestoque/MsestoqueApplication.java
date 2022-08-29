f27d6409acae44e9d74318ce8b3f8784b427e896
moises <moisesestevao2004@hotmail.com>
2022-08-29 16:18:49 -0300

    adding msestoque

---
+++ msestoque/src/main/java/br/com/pagsys/msestoque/MsestoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.pagsys.msestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsestoqueApplication.class, args);
+	}
+
+}
